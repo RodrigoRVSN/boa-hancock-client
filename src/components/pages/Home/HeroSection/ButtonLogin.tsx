5cba68095659de730c4d19c7bf0d648ce5b957c2
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-11-27 12:16:17 -0300

    chore: implement env to API_URL in github auth

--- src/components/pages/Home/HeroSection/ButtonLogin.tsx
+++ src/components/pages/Home/HeroSection/ButtonLogin.tsx
@@ -4,7 +4,7 @@ export const ButtonLogin = () => {
   const { push } = useRouter()
 
   const handleLoginWithGithub = async () => {
-    push('http://localhost:4444/auth/')
+    await push(`${process.env.NEXT_PUBLIC_API_URL}/auth/`)
   }
 
   return (
