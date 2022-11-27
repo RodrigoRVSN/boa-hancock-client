1fc2f8edfa1e129c3ba0c661f84819a2f27be827
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-11-27 12:33:17 -0300

    feat: add responsiveness

--- src/components/pages/Home/HeroSection/index.tsx
+++ src/components/pages/Home/HeroSection/index.tsx
@@ -3,12 +3,12 @@ import { ButtonLogin } from '@components/pages/Home/HeroSection/ButtonLogin'
 
 export const HeroSection = () => {
   return (
-    <div className='bg-black200 w-screen h-screen'>
-      <main className='p-xxlg'>
+    <div className='bg-black200'>
+      <main className='max-w-7xl py-lg mx-auto px-xxs md:px-xs md:px-md min-h-screen'>
         <Image src='/assets/logo.png' alt='Logo Codojo' width={140} height={60}/>
 
-        <section className='flex items-center justify-between'>
-          <div>
+        <section className='flex flex-col md:flex-row items-center mt-xxxlg justify-between'>
+          <div className='text-center md:text-justify md:mr-xxxlg w-full md:w-1/2'>
             <h1 className='text-text font-bold font-space_grotesk text-h1'>A plataforma para encontrar outros
               programadores</h1>
             <p className='text-gray100 text-p1 mt-lg mb-xxlg'>Ache programadores para fazer networking, pair programming
