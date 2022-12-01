22d20fb6a98894284b22056918557bf7c8ebeae9
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-11-30 23:38:57 -0300

    feat: update aside user section

--- src/components/pages/AuthHome/UserSection/index.tsx
+++ src/components/pages/AuthHome/UserSection/index.tsx
@@ -1,18 +1,25 @@
 import Image from 'next/image'
+import { Gear } from 'phosphor-react'
 import { useAppSelector } from '@core/store/hooks'
 
 export const UserSection = () => {
   const { user } = useAppSelector(state => state.user)
 
   return (
-    <div className='flex'>
-      <Image
-        src={user.avatar_url!}
-        alt={`Foto de ${user.name}`}
-        width={64}
-        height={64}
-      />
-      <p>{user.name}</p>
+    <div className='flex justify-between items-center'>
+      <div className='flex items-center gap-md'>
+        <Image
+          src={user.avatar_url!}
+          alt={`Foto de ${user.name}`}
+          width={64}
+          height={64}
+          className='rounded-full'
+        />
+
+        <span className='text-text text-h4'>{user.name}</span>
+      </div>
+
+      <Gear className='mr-xs text-secondary' size={32}/>
     </div>
   )
 }
