702e81d01b3d347b53513a20461b435dff7b3bda
rodrigorvsn <rodrigovictor81@gmail.com>
2023-01-29 15:25:06 -0300

    feat: add padding in mobile messages section

--- src/components/pages/AuthHome/MatchesSection/MatchesList/index.tsx
+++ src/components/pages/AuthHome/MatchesSection/MatchesList/index.tsx
@@ -11,7 +11,7 @@ export const MatchesList = ({ matches, isHorizontal }: MatchesListProps) => {
   if (!matches.length) return <h1 className='text-text'>Opa ninguem aqui</h1>
 
   return (
-    <section className={`flex text-text text-p5  gap-md ${isHorizontal ? 'overflow-auto' : 'flex-wrap'}`}>
+    <section className={`flex text-text text-p5 gap-md p-xxs ${isHorizontal ? 'overflow-auto' : 'flex-wrap'}`}>
       {matches.map(({ matchedUser, id }) => (
         <Link key={id} href={`/messages/${id}`} className={'flex flex-col items-center hover:scale-105 duration-200'}>
           <Image
