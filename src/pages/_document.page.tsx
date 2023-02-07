be2172e2c3f528031dc6213aea447dcb1f7d5319
rodrigorvsn <rodrigovictor81@gmail.com>
2023-02-06 23:27:54 -0300

    chore: add google tag manager script

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -5,6 +5,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PBRBT5S');
+              `
+            }}
+          />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
           <link
